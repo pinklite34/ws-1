fe88dc2f087d40e9af4427493d0663e997bc4c53
Claudia Peart <pinklite36@hotmail.com>
2020-01-31 20:50:05 -0500

    rollup.config.js
    added rollup.config.js

---
+++ rollup.config.js
@@ -0,0 +1,34 @@
+import commonjs from 'rollup-plugin-commonjs'
+import resolve from 'rollup-plugin-node-resolve'
+import typescript from 'rollup-plugin-typescript2'
+import { terser } from 'rollup-plugin-terser'
+import replace from 'rollup-plugin-replace'
+
+export default {
+  input: 'src/WS.ts',
+  output: {
+    file: process.env.BUILD === 'cjs' ? 'dist/ws.js' : 'dist/ws.esm.js',
+    format: process.env.BUILD === 'cjs' ? 'cjs' : 'es',
+    name: 'wspromisify',
+  },
+  sourcemap: true,
+  plugins: [
+    resolve(),
+    commonjs(),
+    typescript({
+      typescript: require("typescript"),
+      tsconfig: "./tsconfig.json",
+      tsconfigOverride: {
+        compilerOptions: {
+          sourceMap: false,
+          inlineSourceMap: process.env.NODE_ENV==='development'
+        }
+      }
+    }),
+    terser(),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify(process.env.BUILD)
+    })
+  ]
+}
+© 2020 GitHub, Inc.
